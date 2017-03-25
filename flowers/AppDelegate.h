497713f08f8931073a7c671158393f26745c86b4
Crazzy <Crazzy@iMac-iMac.local>
2017-03-25 21:07:12 +0300

    project& pod init

---
+++ flowers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flowers
+//
+//  Created by Crazzy on 25.03.17.
+//  Copyright © 2017 Maksim Kis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flowers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  flowers
+//
+//  Created by Crazzy on 25.03.17.
+//  Copyright © 2017 Maksim Kis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
